8d3e9f2a6534fa60c9b12522af9aa0c5a437e67f
tzwsoho <tzwsoho@hotmail.com>
2018-10-11 16:24:46 +0800

    添加工程

---
+++ wxapkg解包/frmMain.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace wxapkg解包
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tvwFileList = new System.Windows.Forms.TreeView();
+            this.lvwFileList = new System.Windows.Forms.ListView();
+            this.colFilename = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colOffset = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnTreeView = new System.Windows.Forms.ToolStripButton();
+            this.btnListView = new System.Windows.Forms.ToolStripButton();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.btnCopy = new System.Windows.Forms.ToolStripButton();
+            this.btnLink1 = new System.Windows.Forms.ToolStripButton();
+            this.gbWXAPKG = new System.Windows.Forms.GroupBox();
+            this.btnWXAPKG = new System.Windows.Forms.Button();
+            this.txtWXAPKG = new System.Windows.Forms.TextBox();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.btnWordWrap = new System.Windows.Forms.ToolStripButton();
+            this.btnBeautifyJS = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.toolStrip2.SuspendLayout();
+            this.gbWXAPKG.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 72);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tvwFileList);
+            this.splitContainer1.Panel1.Controls.Add(this.lvwFileList);
+            this.splitContainer1.Panel1.Controls.Add(this.toolStrip1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtContent);
+            this.splitContainer1.Panel2.Controls.Add(this.toolStrip2);
+            this.splitContainer1.Size = new System.Drawing.Size(981, 450);
+            this.splitContainer1.SplitterDistance = 288;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // tvwFileList
+            // 
+            this.tvwFileList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvwFileList.Location = new System.Drawing.Point(0, 25);
+            this.tvwFileList.Name = "tvwFileList";
+            this.tvwFileList.PathSeparator = "/";
+            this.tvwFileList.ShowNodeToolTips = true;
+            this.tvwFileList.Size = new System.Drawing.Size(288, 425);
+            this.tvwFileList.TabIndex = 1;
+            this.tvwFileList.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.tvwFileList_NodeMouseClick);
+            // 
+            // lvwFileList
+            // 
+            this.lvwFileList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colFilename,
+            this.colSize,
+            this.colOffset});
+            this.lvwFileList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvwFileList.FullRowSelect = true;
+            this.lvwFileList.GridLines = true;
+            this.lvwFileList.HideSelection = false;
+            this.lvwFileList.Location = new System.Drawing.Point(0, 25);
+            this.lvwFileList.MultiSelect = false;
+            this.lvwFileList.Name = "lvwFileList";
+            this.lvwFileList.Size = new System.Drawing.Size(288, 425);
+            this.lvwFileList.TabIndex = 0;
+            this.lvwFileList.UseCompatibleStateImageBehavior = false;
+            this.lvwFileList.View = System.Windows.Forms.View.Details;
+            this.lvwFileList.SelectedIndexChanged += new System.EventHandler(this.lvwFileList_SelectedIndexChanged);
+            // 
+            // colFilename
+            // 
+            this.colFilename.Text = "文件名";
+            this.colFilename.Width = 100;
+            // 
+            // colSize
+            // 
+            this.colSize.Text = "大小";
+            // 
+            // colOffset
+            // 
+            this.colOffset.Text = "偏移";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnTreeView,
+            this.btnListView});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(288, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnTreeView
+            // 
+            this.btnTreeView.Checked = true;
+            this.btnTreeView.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.btnTreeView.Image = ((System.Drawing.Image)(resources.GetObject("btnTreeView.Image")));
+            this.btnTreeView.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnTreeView.Name = "btnTreeView";
+            this.btnTreeView.Size = new System.Drawing.Size(64, 22);
+            this.btnTreeView.Text = "树形框";
+            this.btnTreeView.Click += new System.EventHandler(this.btnViewSwitch_Click);
+            // 
+            // btnListView
+            // 
+            this.btnListView.Image = ((System.Drawing.Image)(resources.GetObject("btnListView.Image")));
+            this.btnListView.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnListView.Name = "btnListView";
+            this.btnListView.Size = new System.Drawing.Size(64, 22);
+            this.btnListView.Text = "列表框";
+            this.btnListView.Click += new System.EventHandler(this.btnViewSwitch_Click);
+            // 
+            // txtContent
+            // 
+            this.txtContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtContent.Location = new System.Drawing.Point(0, 25);
+            this.txtContent.MaxLength = 0;
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ReadOnly = true;
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtContent.Size = new System.Drawing.Size(689, 425);
+            this.txtContent.TabIndex = 0;
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnCopy,
+            this.btnBeautifyJS,
+            this.btnWordWrap,
+            this.btnLink1});
+            this.toolStrip2.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(689, 25);
+            this.toolStrip2.TabIndex = 1;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Image = ((System.Drawing.Image)(resources.GetObject("btnCopy.Image")));
+            this.btnCopy.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(76, 22);
+            this.btnCopy.Text = "复制代码";
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnLink1
+            // 
+            this.btnLink1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.btnLink1.Image = ((System.Drawing.Image)(resources.GetObject("btnLink1.Image")));
+            this.btnLink1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnLink1.Name = "btnLink1";
+            this.btnLink1.Size = new System.Drawing.Size(139, 22);
+            this.btnLink1.Text = "https://beautifier.io";
+            this.btnLink1.ToolTipText = "跳转到 https://beautifier.io";
+            this.btnLink1.Click += new System.EventHandler(this.btnLink1_Click);
+            // 
+            // gbWXAPKG
+            // 
+            this.gbWXAPKG.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbWXAPKG.Controls.Add(this.btnWXAPKG);
+            this.gbWXAPKG.Controls.Add(this.txtWXAPKG);
+            this.gbWXAPKG.Location = new System.Drawing.Point(12, 12);
+            this.gbWXAPKG.Name = "gbWXAPKG";
+            this.gbWXAPKG.Size = new System.Drawing.Size(981, 54);
+            this.gbWXAPKG.TabIndex = 1;
+            this.gbWXAPKG.TabStop = false;
+            this.gbWXAPKG.Text = "选择一个包";
+            // 
+            // btnWXAPKG
+            // 
+            this.btnWXAPKG.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnWXAPKG.Location = new System.Drawing.Point(941, 19);
+            this.btnWXAPKG.Name = "btnWXAPKG";
+            this.btnWXAPKG.Size = new System.Drawing.Size(34, 23);
+            this.btnWXAPKG.TabIndex = 1;
+            this.btnWXAPKG.Text = "...";
+            this.btnWXAPKG.UseVisualStyleBackColor = true;
+            this.btnWXAPKG.Click += new System.EventHandler(this.btnWXAPKG_Click);
+            // 
+            // txtWXAPKG
+            // 
+            this.txtWXAPKG.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtWXAPKG.Location = new System.Drawing.Point(6, 20);
+            this.txtWXAPKG.Name = "txtWXAPKG";
+            this.txtWXAPKG.ReadOnly = true;
+            this.txtWXAPKG.Size = new System.Drawing.Size(929, 21);
+            this.txtWXAPKG.TabIndex = 0;
+            // 
+            // ofd
+            // 
+            this.ofd.DefaultExt = "wxapkg";
+            this.ofd.Filter = "WXAPKG 文件(*.wxapkg)|*.wxapkg|所有文件(*.*)|*.*";
+            // 
+            // btnWordWrap
+            // 
+            this.btnWordWrap.Checked = true;
+            this.btnWordWrap.CheckOnClick = true;
+            this.btnWordWrap.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.btnWordWrap.Image = ((System.Drawing.Image)(resources.GetObject("btnWordWrap.Image")));
+            this.btnWordWrap.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnWordWrap.Name = "btnWordWrap";
+            this.btnWordWrap.Size = new System.Drawing.Size(76, 22);
+            this.btnWordWrap.Text = "自动换行";
+            this.btnWordWrap.Click += new System.EventHandler(this.btnWordWrap_Click);
+            // 
+            // btnBeautifyJS
+            // 
+            this.btnBeautifyJS.CheckOnClick = true;
+            this.btnBeautifyJS.Image = ((System.Drawing.Image)(resources.GetObject("btnBeautifyJS.Image")));
+            this.btnBeautifyJS.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnBeautifyJS.Name = "btnBeautifyJS";
+            this.btnBeautifyJS.Size = new System.Drawing.Size(76, 22);
+            this.btnBeautifyJS.Text = "美化代码";
+            this.btnBeautifyJS.ToolTipText = "此操作可能耗时非常久，慎用！！！";
+            this.btnBeautifyJS.Click += new System.EventHandler(this.btnBeautifyJS_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1005, 534);
+            this.Controls.Add(this.gbWXAPKG);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "wxapkg解包 -- TZWSOHO";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            this.gbWXAPKG.ResumeLayout(false);
+            this.gbWXAPKG.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView lvwFileList;
+        private System.Windows.Forms.ColumnHeader colFilename;
+        private System.Windows.Forms.ColumnHeader colSize;
+        private System.Windows.Forms.ColumnHeader colOffset;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.GroupBox gbWXAPKG;
+        private System.Windows.Forms.Button btnWXAPKG;
+        private System.Windows.Forms.TextBox txtWXAPKG;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.TreeView tvwFileList;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnTreeView;
+        private System.Windows.Forms.ToolStripButton btnListView;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripButton btnCopy;
+        private System.Windows.Forms.ToolStripButton btnLink1;
+        private System.Windows.Forms.ToolStripButton btnWordWrap;
+        private System.Windows.Forms.ToolStripButton btnBeautifyJS;
+    }
+}
+
---
+++ wxapkg解包/frmMain.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+using JSBeautifyLib;
+
+namespace wxapkg解包
+{
+    public partial class frmMain : Form
+    {
+        #region WXAPKG 文件结构
+
+        [StructLayout(LayoutKind.Explicit)]
+        private class WXAPKG_HEADER
+        {
+            [MarshalAs(UnmanagedType.U1), FieldOffset(0)]
+            internal byte m_first_mark;
+            [MarshalAs(UnmanagedType.I4), FieldOffset(1)]
+            internal int m_edition;
+            [MarshalAs(UnmanagedType.I4), FieldOffset(5)]
+            internal int m_index_info_length;
+            [MarshalAs(UnmanagedType.I4), FieldOffset(9)]
+            internal int m_body_info_length;
+            [MarshalAs(UnmanagedType.U1), FieldOffset(13)]
+            internal byte m_last_mark;
+            [MarshalAs(UnmanagedType.I4), FieldOffset(14)]
+            internal int m_file_count;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private class WXAPKG_FILE
+        {
+            internal int m_name_length;
+            internal string m_name;
+            internal int m_offset;
+            internal int m_size;
+        }
+
+        #endregion
+
+        public string FormatCapability(UInt64 nCapability, int nDigits = 2)
+        {
+            string szCapability = "";
+            if (nCapability < 1024)
+            {
+                szCapability = nCapability + " B";
+            }
+            else if (nCapability < 1024 * 1024)
+            {
+                szCapability = (nCapability / 1024.0).ToString("F" + nDigits) + " KB";
+            }
+            else if (nCapability < 1024 * 1024 * 1024)
+            {
+                szCapability = (nCapability / 1024.0 / 1024.0).ToString("F" + nDigits) + " MB";
+            }
+            else
+            {
+                szCapability = (nCapability / 1024.0 / 1024.0 / 1024.0).ToString("F" + nDigits) + " GB";
+            }
+
+            return szCapability;
+        }
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWXAPKG_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK != ofd.ShowDialog())
+            {
+                return;
+            }
+
+            txtContent.Text = "";
+            txtWXAPKG.Text = ofd.FileName;
+            lvwFileList.Items.Clear();
+            tvwFileList.Nodes.Clear();
+
+            // WXAPKG 格式：https://toutiao.io/posts/33fum8/preview
+
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(txtWXAPKG.Text, FileMode.Open, FileAccess.Read);
+
+                // 获取头部信息
+                WXAPKG_HEADER header = new WXAPKG_HEADER();
+
+                // 第一个标识，固定为 0xBE
+                header.m_first_mark = (byte)fs.ReadByte();
+                if (0xBE != header.m_first_mark)
+                {
+                    throw new Exception("文件第一个标识有误，非 WXAPKG 格式文件！");
+                }
+
+                // 版本号，0 -> 微信分发到客户端 1 -> 开发者上传到微信后台
+                byte[] b_int = new byte[4];
+                fs.Read(b_int, 0, b_int.Length);
+                header.m_edition = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(b_int, 0));
+
+                // 索引段的长度
+                fs.Read(b_int, 0, b_int.Length);
+                header.m_index_info_length = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(b_int, 0));
+
+                // 数据段的长度
+                fs.Read(b_int, 0, b_int.Length);
+                header.m_body_info_length = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(b_int, 0));
+
+                // 第二个标识，固定为 0xED
+                header.m_last_mark = (byte)fs.ReadByte();
+                if (0xED != header.m_last_mark)
+                {
+                    throw new Exception("文件标识有误，非 WXAPKG 格式文件！");
+                }
+
+                // 文件数量
+                fs.Read(b_int, 0, b_int.Length);
+                header.m_file_count = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(b_int, 0));
+
+                tvwFileList.BeginUpdate();
+                TreeNode tn_root = tvwFileList.Nodes.Add("/");
+
+                lvwFileList.BeginUpdate();
+
+                // 获取每个文件的信息
+                for (int i = 0; i < header.m_file_count; i++)
+                {
+                    WXAPKG_FILE file = new WXAPKG_FILE();
+
+                    // 文件长度
+                    fs.Read(b_int, 0, b_int.Length);
+                    file.m_name_length = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(b_int, 0));
+
+                    if (file.m_name_length > 255)
+                    {
+                        throw new Exception("错误的文件长度！");
+                    }
+
+                    // 文件名
+                    byte[] b_name = new byte[file.m_name_length];
+                    fs.Read(b_name, 0, b_name.Length);
+                    file.m_name = Encoding.UTF8.GetString(b_name);
+
+                    // 偏移
+                    fs.Read(b_int, 0, b_int.Length);
+                    file.m_offset = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(b_int, 0));
+
+                    // 大小
+                    fs.Read(b_int, 0, b_int.Length);
+                    file.m_size = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(b_int, 0));
+
+                    // 添加到列表框
+                    ListViewItem lvi = new ListViewItem(file.m_name);
+                    lvi.SubItems.Add(FormatCapability((UInt64)file.m_size));
+                    lvi.SubItems.Add(file.m_offset.ToString());
+                    lvi.Tag = file;
+
+                    lvwFileList.Items.Add(lvi);
+
+                    // 添加到树形框
+                    AddTreeNode(tn_root, file.m_name, file);
+                }
+
+                lvwFileList.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                lvwFileList.EndUpdate();
+
+                tvwFileList.ExpandAll();
+                tvwFileList.EndUpdate();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+
+        private void AddTreeNode(TreeNode tn_parent, string file_name, WXAPKG_FILE file)
+        {
+            TreeNode tn = null;
+            string[] arr_path = file_name.Split("/".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            if (tn_parent.Nodes.ContainsKey(arr_path[0]))
+            {
+                tn = tn_parent.Nodes[arr_path[0]];
+            }
+            else
+            {
+                tn = new TreeNode(arr_path[0]);
+                tn.Name = arr_path[0];
+                tn_parent.Nodes.Add(tn);
+            }
+
+            file_name = string.Join("/", arr_path);
+            if (file_name.IndexOf('/', 1) < 0)
+            {
+                tn.Tag = file;
+                tn.ToolTipText =
+                    "大小：" + FormatCapability((UInt64)file.m_size) +
+                    " 偏移：" + file.m_offset;
+                return;
+            }
+
+            file_name = file_name.Substring(file_name.IndexOf('/', 1));
+            AddTreeNode(tn, file_name, file);
+        }
+
+        private void ShowFileContent(WXAPKG_FILE file)
+        {
+            if ("" == txtWXAPKG.Text ||
+                null == file)
+            {
+                return;
+            }
+
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(txtWXAPKG.Text, FileMode.Open, FileAccess.Read);
+
+                byte[] b_content = new byte[file.m_size];
+                fs.Seek((long)file.m_offset, SeekOrigin.Begin);
+                fs.Read(b_content, 0, file.m_size);
+                fs.Close();
+
+                string content = Encoding.UTF8.GetString(b_content);
+                if (btnBeautifyJS.Checked &&
+                    (file.m_name.EndsWith(".js") ||
+                     file.m_name.EndsWith(".json")))
+                {
+                    JSBeautifyOptions jsbo = new JSBeautifyOptions();
+                    JSBeautify jsb = new JSBeautify(content, jsbo);
+                    content = jsb.GetResult();
+                }
+
+                txtContent.Text = content;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+
+        private void lvwFileList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (1 != lvwFileList.SelectedItems.Count)
+            {
+                return;
+            }
+
+            WXAPKG_FILE file = lvwFileList.SelectedItems[0].Tag as WXAPKG_FILE;
+            if (null == file)
+            {
+                return;
+            }
+
+            ShowFileContent(file);
+        }
+
+        private void tvwFileList_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (null == e.Node ||
+                e.Node.Nodes.Count > 0)
+            {
+                return;
+            }
+
+            WXAPKG_FILE file = e.Node.Tag as WXAPKG_FILE;
+            if (null == file)
+            {
+                return;
+            }
+
+            ShowFileContent(file);
+        }
+
+        private void btnViewSwitch_Click(object sender, EventArgs e)
+        {
+            btnListView.Checked = false;
+            btnTreeView.Checked = false;
+            ((ToolStripButton)sender).Checked = true;
+
+            if (sender == btnListView)
+            {
+                lvwFileList.BringToFront();
+            }
+            else
+            {
+                tvwFileList.BringToFront();
+            }
+        }
+
+        private void btnWordWrap_Click(object sender, EventArgs e)
+        {
+            txtContent.WordWrap = btnWordWrap.Checked;
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.Clear();
+            Clipboard.SetText(txtContent.Text);
+            Application.DoEvents();
+
+            MessageBox.Show("已复制到剪贴板！");
+        }
+
+        private void btnLink1_Click(object sender, EventArgs e)
+        {
+            Process.Start("Explorer.exe", "https://beautifier.io");
+        }
+
+        private void btnBeautifyJS_Click(object sender, EventArgs e)
+        {
+            if (btnBeautifyJS.Checked)
+            {
+                if (DialogResult.Yes != MessageBox.Show(
+                    "此操作可能导致界面卡死，是否确认使用？", "使用确认",
+                    MessageBoxButtons.YesNo))
+                {
+                    btnBeautifyJS.Checked = false;
+                    return;
+                }
+            }
+        }
+    }
+}
